3dc1749b330a2d212fc30c29717fa026baed516b
TayehTech <yacoub.altayeh@icloud.com>
2024-08-26 23:28:17 +0300

    Makefile

--- ft_putstr_fd.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-void ft_putstr_fd(char *s, int fd)
-{
-    size_t len;
-
-    len = strlen(s);
-    write(fd, s, len);
-}
--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "libft.h"
-
-int main()
-{
-    char *num;
-    num = ft_itoa(0);
-    printf("%s\n", num);
-}
